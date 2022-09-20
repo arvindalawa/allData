86d9823a8aa65176e834336802f3ce8c44833766
afaroj sayyad <afaroj.geexu@gmail.com>
2022-09-20 22:16:34 +0530

    Accordion

--- force-app/main/default/lwc/accordion/accordion-data.js
+++ force-app/main/default/lwc/accordion/accordion-data.js
@@ -2,8 +2,8 @@ export const data = [
 	{
 		id: 1,
 		title_label: 'Scoping Questions 1',
-		isVisible: 'true',
-		activeSections: ['A', 'B', 'C'],
+		isVisible: true,
+		activeSections: ['A', 'B'],
 		accordionData: [
 			{
 				id: 1,
@@ -66,6 +66,134 @@ export const data = [
 					},
 					{
 						menu_label: 'Menu c',
+						link: 'www.google.com',
+					},
+				],
+
+			},
+		],
+
+	},
+	{
+		id: 2,
+		title_label: 'Scoping Questions 2',
+		isVisible: false,
+		activeSections: ['2'],
+		accordionData: [
+			{
+				id: 1,
+				label: 'Accordion Title 1',
+				subtext: '#Number of users',
+				name: '1',
+				para: 'Dummy text Donec vitae tellus egestas, faucibus ipsum ac, imperdiet erat. Nam venenatis non ante at sagittis.',
+				menu: [
+					{
+						menu_label: 'Menu 1',
+						link: 'www.google.com',
+					},
+					{
+						menu_label: 'Menu 2',
+						link: 'www.google.com',
+					},
+					{
+						menu_label: 'Menu 3',
+						link: 'www.google.com',
+					},
+				],
+
+			},
+			{
+				id: 2,
+				label: 'Accordion Title 2',
+				subtext: '#Number of users',
+				name: '2',
+				para: 'Dummy text Donec vitae tellus egestas, faucibus ipsum ac, imperdiet erat. Nam venenatis non ante at sagittis.',
+				menu: [
+					{
+						menu_label: 'Menu A',
+						link: 'www.google.com',
+					},
+					{
+						menu_label: 'Menu B',
+						link: 'www.google.com',
+					},
+					{
+						menu_label: 'Menu C',
+						link: 'www.google.com',
+					},
+				],
+
+			},
+			{
+				id: 3,
+				label: 'Accordion Title 3',
+				subtext: '#Number of users',
+				name: '3',
+				para: 'Dummy text Donec vitae tellus egestas, faucibus ipsum ac, imperdiet erat. Nam venenatis non ante at sagittis.',
+				menu: [
+					{
+						menu_label: 'Menu a',
+						link: 'www.google.com',
+					},
+					{
+						menu_label: 'Menu b',
+						link: 'www.google.com',
+					},
+					{
+						menu_label: 'Menu c',
+						link: 'www.google.com',
+					},
+				],
+
+			},
+		],
+
+	},
+	{
+		id: 3,
+		title_label: 'Scoping Questions 3',
+		isVisible: true,
+		activeSections: ['002'],
+		accordionData: [
+			{
+				id: 1,
+				label: 'Accordion Title 001',
+				subtext: '#Number of users',
+				name: '001',
+				para: 'Dummy text Donec vitae tellus egestas, faucibus ipsum ac, imperdiet erat. Nam venenatis non ante at sagittis.',
+				menu: [
+					{
+						menu_label: 'Menu 1',
+						link: 'www.google.com',
+					},
+					{
+						menu_label: 'Menu 2',
+						link: 'www.google.com',
+					},
+					{
+						menu_label: 'Menu 3',
+						link: 'www.google.com',
+					},
+				],
+
+			},
+			{
+				id: 2,
+				label: 'Accordion Title 002',
+				subtext: '#Number of users',
+				name: '002',
+				para: 'Dummy text Donec vitae tellus egestas, faucibus ipsum ac, imperdiet erat. Nam venenatis non ante at sagittis.',
+				menu: [
+					{
+						menu_label: 'Menu A',
+						link: 'www.google.com',
+					},
+					{
+						menu_label: 'Menu B',
+						link: 'www.google.com',
+					},
+					{
+						menu_label: 'Menu C',
 						link: 'www.google.com',
 					},
 				],
--- force-app/main/default/lwc/accordion/accordion.js
+++ force-app/main/default/lwc/accordion/accordion.js
@@ -5,29 +5,51 @@ export default class LightningExampleAccordionMultiple extends LightningElement 
 @track activeSections = [];
 activeSectionsMessage = '';
 @api accordion = [];
-@track isVisible = false;
+@track isVisible;
+@track clickedButtonLabel = 'utility:chevrondown';
+
+handleClick(event) {
+	if (!this.activeSections.length) {
+		this.activeSections = [];
+		this.accordion.forEach((item) => {
+			if (item.id == event.target.dataset.id) {
+				this.clickedButtonLabel = 'utility:chevronup';
+
+				item.accordionData.forEach((accord) => {
+					this.activeSections.push(accord.name);
+				});
+			}
+		});
+	} else {
+		this.clickedButtonLabel = 'utility:chevrondown';
+		this.activeSections = [];
+	}
+}
 
 connectedCallback() {
 	this.accordion = data;
-}
-handleSectionToggle(event) {
-	const { openSections } = event.detail;
-	if (openSections.length === 0) {
-		this.activeSectionsMessage = 'All sections are closed';
-	} else {
-		this.activeSectionsMessage = `Open sections: ${openSections.join(', ')}`;
-	}
+	this.getActiveSections();
 }
 getactiveSectionName() {
 	for (let i = 0; i <= this.accordion.length; i++) {
 		this.activeSections = this.accordion[i].activeSections;
 	}
 }
-expandAll() {
-	this.activeSections = ['A', 'B', 'C'];
-}
-
-collapseAll() {
+getActiveSections(event) {
 	this.activeSections = [];
+	this.accordion.forEach((activeItem) => {
+		if (activeItem.isVisible === true) {
+			this.activeSections = [...this.activeSections, ...activeItem.activeSections];
+		} else if (activeItem.isVisible === true && activeItem.activeSections.length === 0) {
+			this.activeSections = [];
+			this.accordion.forEach((item) => {
+				if (item.id == event.target.dataset.id) {
+					item.accordionData.forEach((accord) => {
+						this.activeSections.push(accord.name);
+					});
+				}
+			});
+		}
+	});
 }
 }
